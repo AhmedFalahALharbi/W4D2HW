28adc98ee257ff48edce2c5eda7737c253f13fdb
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-09-17 03:16:01 +0300

    add bonus

--- Profile.js
+++ Profile.js
@@ -5,3 +5,12 @@ let fullName = localStorage.getItem('fullName');
         } else {
             location.assign('index.html');
         }
+
+ let logoutBtn = document.getElementById('logoutbtn');
+        logoutBtn.addEventListener('click', () => {
+             
+            localStorage.removeItem('fullName');
+        
+                
+            location.assign('index.html');
+        });
